6a7983de028a9b5b0fccf11c841522034bf7b1dd
Riley Slayden <slaydenriley@gmail.com>
2020-02-27 18:27:44 +0000

    Automatically backed up by Learn

--- db/migrate/20160112163253_create_songs.rb
+++ db/migrate/20160112163253_create_songs.rb
@@ -3,6 +3,7 @@ class CreateSongs < ActiveRecord::Migration
     create_table :songs do |t|
       t.string :title
       t.integer :artist_id
+      t.integer :genre_id
 
       t.timestamps null: false
     end
